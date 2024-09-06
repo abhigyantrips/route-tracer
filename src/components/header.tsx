a39c650b4856babd5194aa23f81afe65a4de190e
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-09-06 09:34:59 +0530

    fix(ui): add tooltip to theme switcher

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 export function Header() {
   return (
     <header className="sticky inset-0 top-0 z-50 flex h-16 w-full flex-col items-center justify-around border-b bg-background/60 backdrop-blur-[5px] backdrop-saturate-[180%]">
-      <nav className="container relative flex w-full flex-1 items-center">
-        <div className="flex w-full items-center">
+      <nav className="container relative flex w-full flex-1 items-center justify-between">
+        <div className="flex items-center">
           <h1 className="text-lg font-bold">Route Tracer</h1>
         </div>
         <ThemeSwitcher />
--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,21 +1,37 @@
+"use client";
+
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="rounded-full"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-      <Moon className="hidden h-5 w-5 dark:block" />
-      <span className="sr-only">Switch Theme</span>
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="rounded-full"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+            <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+            <Moon className="hidden h-5 w-5 dark:block" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Toggle theme</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
